c8f0fec8a22820605938ca9e2ee6c57d901e71a8
Jake VanEssendelft <jrv11706@uga.edu>
2024-09-24 13:46:15 -0400

    Having revelations on what this is supposed to be

--- TopLevel/SCC_Wrapper.v
+++ TopLevel/SCC_Wrapper.v
@@ -15,17 +15,17 @@ begin
     output          data_write;      // control writing data
 
     // Inner layer 
-    output          clk0;            // main clock signal
-    output          reset;           // sets all regs to known state
-    output [31:0]   in_mem;          // instructions being fetched
-    output [31:0]   data_in;         // data read from memory
+    output          scc_clk;            // main clock signal
+    output          scc_reset;           // sets all regs to known state
+    output [31:0]   scc_in_mem;          // instructions being fetched
+    output [31:0]   scc_data_in;         // data read from memory
 
-    input [31:0]    in_mem_addr;     // address pointed to in instruction memory
-    input           in_mem_en;       // enable instruction memory fetch
-    input [31:0]    data_addr;       // address pointed to in data memory
-    input [31:0]    data_out;        // data to write to memory
-    input           data_read;       // control reading data
-    input           data_write;      // control writing data
+    input [31:0]    scc_in_mem_addr;     // address pointed to in instruction memory
+    input           scc_in_mem_en;       // enable instruction memory fetch
+    input [31:0]    scc_data_addr;       // address pointed to in data memory
+    input [31:0]    scc_data_out;        // data to write to memory
+    input           scc_data_read;       // control reading data
+    input           scc_data_write;      // control writing data
 
     assign clk0 = clk;
 
