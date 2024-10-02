073207d86f1671c0100fa61380e189e040543f71
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-02 17:12:58 -0400

    Updated some things with Vinessa

--- IF-ID Checkpoint/ID.v
+++ IF-ID Checkpoint/ID.v
@@ -1,5 +1,11 @@
 module ID();
 
+    input instruction;
+
+    reg [1:0] 1LD;
+    reg S;
+    reg [3:0] 2LD;
+    reg [2:0] ALU_OC; 
     
 
 endmodule
--- IF-ID Checkpoint/IF.v
+++ IF-ID Checkpoint/IF.v
@@ -1,30 +1,34 @@
+/*
+ * This module defines the Instruction Fetch (IF) implementation by the Linux SCC group. IF keeps track
+ * of the program counter (PC) which points to a word address in Instruction Memory (IM). 
+ */
 module IF(clk, b_relAddr, b_absAddr, b_cond, b_abs, pc);
 
     input               clk;            // Clock signal
     input               b_cond;         // Branch Condition Status
-    input               b_abs;          // Branch is Absolute or not
+    input               b_abs;          // Branch is Absolute or not, i.e. branch to target addr
     input [15:0]        b_relAddr;      // Amount to branch relative to current PC
     input [31:0]        b_absAddr;      // Address to branch to on absolute branch (to value in register)
 
     output reg [31:0]   pc;             // Program Counter: points to an address in instruction memory
 
-    always @(posedge clk)
+    always @(posedge clk)               // Ensures that PC only changes on rising clock edge
     begin
 
-        if (b_cond)
+        if (b_cond)                        // check if branch condition is met i.e. the b_cond flag is set by execution stage
         begin
-            if (b_abs)
+            if (b_abs)                     // check if absolute  branch
             begin
-                pc = b_absAddr;
-            end else
+                pc = b_absAddr;            // if abs branch, then set pc = target address
+            end else                       // else, then relative branch
             begin
-                pc = pc + (4 * b_relAddr);
+                pc = pc + (4 * b_relAddr); // relative address is shifted left by 2 bits and pc is set to this value
             end
-        end else
+        end else                           // branch condition not met i.e. no branch
         begin
-            pc = pc + 4;
+            pc = pc + 4;                   // incrementing, pc by 4 bytes 
         end
 
     end
 
-endmodule
+endmodule 
