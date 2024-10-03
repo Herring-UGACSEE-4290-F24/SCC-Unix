2f44738409ae38aa6187f464d26007764bcb0d99
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-03 17:33:26 -0400

    updated the IF to be able to handle offsets

--- IF-ID Checkpoint/ID.v
+++ IF-ID Checkpoint/ID.v
@@ -1,11 +1,8 @@
 module ID();
 
-    input instruction;
+    input [31:0] instruction;
 
-    reg [1:0] 1LD;
-    reg S;
-    reg [3:0] 2LD;
-    reg [2:0] ALU_OC; 
+    
     
 
 endmodule
--- IF-ID Checkpoint/IF.v
+++ IF-ID Checkpoint/IF.v
@@ -2,13 +2,14 @@
  * This module defines the Instruction Fetch (IF) implementation by the Linux SCC group. IF keeps track
  * of the program counter (PC) which points to a word address in Instruction Memory (IM). 
  */
-module IF(clk, b_relAddr, b_absAddr, b_cond, b_abs, pc);
+module IF(clk, b_relAddr, b_absAddr, b_cond, b_abs, offset, pc);
 
     input               clk;            // Clock signal
     input               b_cond;         // Branch Condition Status
     input               b_abs;          // Branch is Absolute or not, i.e. branch to target addr
     input [15:0]        b_relAddr;      // Amount to branch relative to current PC
     input [31:0]        b_absAddr;      // Address to branch to on absolute branch (to value in register)
+    input [15:0]        offset;         // Optional offset for the branch address
 
     output reg [31:0]   pc;             // Program Counter: points to an address in instruction memory
 
@@ -19,7 +20,7 @@ module IF(clk, b_relAddr, b_absAddr, b_cond, b_abs, pc);
         begin
             if (b_abs)                     // check if absolute  branch
             begin
-                pc = b_absAddr;            // if abs branch, then set pc = target address
+                pc = b_absAddr + offset;   // if abs branch, then set pc = target address
             end else                       // else, then relative branch
             begin
                 pc = pc + (4 * b_relAddr); // relative address is shifted left by 2 bits and pc is set to this value
