e4a7c465dc8c4d03bb7da88f276abc3478a94c26
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-08 11:55:24 -0400

    edited comments for IF.v

--- IF-ID Checkpoint/IF.v
+++ IF-ID Checkpoint/IF.v
@@ -4,14 +4,14 @@
  */
 module IF(clk, b_relAddr, b_cond, offset, instruction_in, instruction_out, pc);
 
-    input               clk;            // Clock signal
-    input               b_cond;         // Branch Condition StatusS
-    input [15:0]        b_relAddr;      // Amount to branch relative to current PC
-    input [31:0]        b_absAddr;      // Address to branch to on absolute branch (to value in register)
-    input [15:0]        offset;         // Optional offset for the branch address
+    input               clk;             // Clock signal
+    input               b_cond;          // Branch Condition StatusS
+    input [15:0]        b_relAddr;       // Amount to branch relative to current PC
+    input [31:0]        b_absAddr;       // Address to branch to on absolute branch (to value in register)
+    input [15:0]        offset;          // Optional offset for the branch address
 
-    input [31:0] instruction_in;
-    output [31:0] instruction_out;
+    input [31:0]	instruction_in;  // Retrieved from Instruction Memory
+    output [31:0]       instruction_out; // Passing onto Instruction Decode
 
     output reg [31:0]   pc;             // Program Counter: points to an address in instruction memory
 
@@ -23,21 +23,17 @@ module IF(clk, b_relAddr, b_cond, offset, instruction_in, instruction_out, pc);
         end else if (instruction_in[31:25] == 'b1100010) begin   // BR instruction
             pc <=                                                // change to address in register
             instruction_out <= NOP;
-        end else begin
-            instruction_out <= instruction_in;
-            if (b_cond)
+        end else begin                                           // When not a branch instruction
+            instruction_out <= instruction_in;                   // Pass instruction through to ID component
+            if (b_cond)                                          // If conditional branch is decoded
             begin
-                pc <= pc + (4 * b_relAddr);
+                pc <= pc + (4 * b_relAddr);                          // Branch according to the conditional branch statement
             end else
             begin
-                pc <= pc + 4;
+                pc <= pc + 4;                                        // Otherwise, increment the PC
             end
         end 
 
     end
 
-endmodule 
-
-// Change to the other layout, where instructions are returned back to here, the IF
-
-// Then we can check for unconditional branches, and immediately handle them so that ID doesn't see a b
+endmodule
