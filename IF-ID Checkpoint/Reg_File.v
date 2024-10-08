aeac41564c5dec40e4b19ed8450ab6552a1621b1
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-08 00:08:09 -0400

    Updated register file to have base functionality

--- IF-ID Checkpoint/Reg_File.v
+++ IF-ID Checkpoint/Reg_File.v
@@ -1,14 +1,24 @@
-module Reg_File()
+module Reg_File(read_addr1, read_addr2, write_addr, write_value, write_enable, clk, value1, value2);
 
-    reg [2:0] registers [31:0];
-    input [2:0] in_addr;
+    input [2:0] read_addr1, read_addr2;
+    input [2:0] write_addr;
+    input [31:0] write_value;
+    input write_enable;
+
     input clk;
 
-    output [31:0] out;
+    output [31:0] value1, value2;
+
+    reg [2:0] registers [31:0];
 
     always @(posedge clk) begin
 
-        out <= registers[in_addr];
+        value1 <= registers[read_addr1];
+        value2 <= registers[read_addr2];
+
+        if (write_enable) begin
+            registers[write_addr] <= write_value
+        end
 
     end
 
