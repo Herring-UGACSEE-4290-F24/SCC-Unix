5404b98ec6a8c593861318ff12128d0cbc7da54d
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-11 04:58:57 -0400

    fixing errors

--- EXE.v
+++ EXE.v
@@ -3,7 +3,7 @@ module EXE(value1, value2, immediate, alu_oc, ir_op, result);
     input [31:0]        value1, value2, immediate; 
     input [2:0]         alu_oc;
     input               ir_op;
-    output reg [31:0]   result;
+    output wire [31:0]   result;
 
     reg [31:0]          op2;
     ALU alu1(
@@ -13,7 +13,7 @@ module EXE(value1, value2, immediate, alu_oc, ir_op, result);
         .result (result)
     );
 
-    always @(posedge clk)
+    always @(*)
     begin
 
         if (ir_op)              // Loads immediate into the ALU on 0, and the register value on 1
