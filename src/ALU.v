1eb990fbeb78638c4abf287ce822046f0fcd106c
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-23 21:15:32 -0400

    adding some fixes to ALU

--- src/ALU.v
+++ src/ALU.v
@@ -1,17 +1,17 @@
 // Module declaration
-module ALU(operand1, operand2, opcode, result, overflow);
+module ALU(operand1, operand2, opcode, res_out, overflow);
 
 // ALU inputs
 input [2:0] opcode;
-input [31:0] operand1; 
-input [31:0] operand2;
+input [32:0] operand1; 
+input [3:0] operand2;
 
 // ALU outputs
-output reg [32:0] result;
+reg [32:0] result;
+output [31:0] res_out;
 output reg overflow;
 
-// ALU Operation Commands
-// NOP: 000
+// ALU Operation Commands //
 // ADD: 001
 // SUB: 010
 // AND: 011
@@ -25,45 +25,40 @@ parameter OR = 3'b100, XOR = 3'b101, NOT = 3'b110;
 always @(*) begin
         
     case (opcode)
-    // Addition
-    ADD: begin
+    ADD: begin // Addition
         result = operand1 + operand2;
-        overflow <= result[32];
     end
 
-    // Subtraction
-    SUB: begin
+    SUB: begin // Subtraction
         result = operand1 - operand2;
-        overflow <= result[32];
     end
 
-    // Bitwise AND
-    AND: begin
+    AND: begin // Bitwise AND
         result <= operand1 & operand2;
     end
 
-    // Bitwise OR
-    OR: begin
+    OR: begin // Bitwise OR
         result <= operand1 | operand2;
     end
 
-    // Bitwise XOR
-    XOR: begin
+    XOR: begin // Bitwise XOR
         result <= operand1 ^ operand2;
     end
 
-    // Bitwise NOT
-    NOT: begin
+    NOT: begin // Bitwise NOT
         result <= ~operand1;
     end
 
-    // No valid opcode
-    default: begin
+    default: begin // No valid opcode
         result <= 0;
         overflow <= 0;
     end
 
     endcase
+
+    res_out = result[31:0];
+    overflow = result[31:0];
+
 end
 
 endmodule
