eac40bbea378710c40ad78d14a33e44043c31e8b
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-26 13:58:44 -0400

    fixed the double instruction problem, and helped with the branch issue a little

--- src/IF.v
+++ src/IF.v
@@ -25,9 +25,8 @@ module IF(clk, reset, br_value, instruction_in, instruction_out, br_addr, re_pc_
         instruction_out = 0;
     end
 
-    always @(posedge clk) begin
+    always @(*) begin
 
-        wr_pc_val = 0;
     // FOR HANDLING THE IMMEDIATE/OFFSET FIELD IN B and BR //
     // =================================================== //
     // This will grab the lower 16 bits of the instruction //
@@ -36,7 +35,6 @@ module IF(clk, reset, br_value, instruction_in, instruction_out, br_addr, re_pc_
     // - - - - - - - - - - - - - - - - - - - - - - - - - - //
         offset[31:16] = {16{instruction_in[15]}};          // Duplicates the msb (sign extension)
         offset[15:0] = instruction_in[15:0];               // Copying the immediate value
-        offset = offset * 4;                               // Left shifts (4 byte alligned)
     // =================================================== //
 
     // FOR HANDLING BRANCHES THAT ARE UNCONDITIONAL //
@@ -58,11 +56,11 @@ module IF(clk, reset, br_value, instruction_in, instruction_out, br_addr, re_pc_
     // ==================================================== //
 
         instruction_out = prefetch;
-        if (instruction_in[31:25] == 'b1100000) begin
+        if (instruction_in[31:25] == 7'b1100000) begin
 
             wr_pc_val = re_pc_val + offset;         // Update the pc based on the instruction's offset
 
-        end else if (instruction_in[31:25] == 'b1100010) begin
+        end else if (instruction_in[31:25] == 7'b1100010) begin
 
             br_addr = instruction_in[24:22];        // Uses bitfield to fetch address of register
             wr_pc_val = br_value + offset;          // Update pc to address in the register pointed to by br_addr +/- the offset
