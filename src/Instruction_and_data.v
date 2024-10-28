2fcb45b1f07428a306524166d1c94846bb5b98ef
Jake VanEssendelft <jrv11706@uga.edu>
2024-10-28 10:46:09 -0400

    Updated memory model because it was stinky before

--- src/Instruction_and_data.v
+++ src/Instruction_and_data.v
@@ -16,22 +16,29 @@ initial begin
   $readmemh("output.mem", memory);
 end
 
-always @(mem_Clk) begin
-    if(instruction_memory_en)begin //Grabs 32 bit instruction
+always @(instruction_memory_a) begin
+  if(instruction_memory_en)begin //Grabs 32 bit instruction
     instruction_memory_v[31:24] <= memory[instruction_memory_a];
     instruction_memory_v[23:16] <= memory[instruction_memory_a+1];
     instruction_memory_v[15:8] <= memory[instruction_memory_a+2];
     instruction_memory_v[7:0] <= memory[instruction_memory_a+3];
-    end
-    else if (~instruction_memory_en) begin //When low the SCC program pauses until set back to high which continues fetching instructions
+  end
+
+  else if (~instruction_memory_en) begin //When low the SCC program pauses until set back to high which continues fetching instructions
     instruction_memory_v <= 'hFFFFFFFF;
-    end
+  end
+end
+
+always @(data_memory_a) begin
   if(data_memory_read) begin //Load instruction
     data_memory_in_v[31:24] <=memory[data_memory_a];
     data_memory_in_v[23:16] <=memory[data_memory_a+1];
     data_memory_in_v[15:8] <=memory[data_memory_a+2];
     data_memory_in_v[7:0] <=memory[data_memory_a+3];
   end
+end
+
+always @(posedge mem_Clk) begin   // PROBLEM: this wasn't on *posedge* initially
   if(data_memory_write) begin //Store instruction
     memory[data_memory_a] <= data_memory_out_v[31:24];
     memory[data_memory_a+1] <= data_memory_out_v[23:16];
@@ -41,4 +48,3 @@ always @(mem_Clk) begin
   end
 end
 endmodule
-
